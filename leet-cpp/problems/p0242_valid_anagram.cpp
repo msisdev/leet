71d77b20d5281c320111e9a6b4e54408cad185e3
msisdev <33077839+msisdev@users.noreply.github.com>
2026-07-22 12:09:14 +0900

    0242 valid-anagram

---
+++ leet-cpp/problems/p0242_valid_anagram.cpp
@@ -0,0 +1,16 @@
+#include "problems/p0242_valid_anagram.hpp"
+
+#include <vector>
+
+class Solution {
+public:
+    // TODO: Implement your solution method.
+    int solve(const std::vector<int>& nums) {
+        (void)nums;
+        return 0;
+    }
+};
+
+void run_p0242_demo() {
+    // TODO: Add local demo cases if needed.
+}
