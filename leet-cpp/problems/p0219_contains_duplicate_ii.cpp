ddad1d5639923dff97cd67f35983071eec5c08e9
msisdev <33077839+msisdev@users.noreply.github.com>
2026-07-25 19:12:09 +0900

    0219 contains duplicate ii: use fixed length window

---
+++ leet-cpp/problems/p0219_contains_duplicate_ii.cpp
@@ -0,0 +1,34 @@
+#include "problems/p0219_contains_duplicate_ii.hpp"
+
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> num_set(nums.begin(), nums.end());
+        int res = 0;
+
+        for (int num : num_set) {
+            if (!num_set.count(num - 1)) {
+                int i = 1;
+
+                while (num_set.count(num + i)) {
+                    i += 1;
+                }
+
+                res = std::max(res, i);
+            }
+        }
+
+        return (res);
+    }
+};
+
+void run_p0219_demo() {
+    // TODO: Add local demo cases if needed.
+}
