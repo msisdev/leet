7d0922d2a2f5946c2abce32ecbae1c15fcac852a
msisdev <33077839+msisdev@users.noreply.github.com>
2026-08-04 12:27:21 +0900

    0030 substring with concatenation of all words

--- leet-cpp/problems/p0030_substring_with_concatenation_of_all_words.cpp
+++ leet-cpp/problems/p0030_substring_with_concatenation_of_all_words.cpp
@@ -3,28 +3,92 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <optional>
 
 using namespace std;
 
+class Window {
+private:
+    unordered_map<string, int> target_map;
+    int target_count;
+    unordered_map<string, int> window_map;
+    int window_count;
+
+public:
+    Window(const unordered_map<string, int>& target_map, int target_count)
+    : target_map(target_map), target_count(target_count), window_count(0) {}
+
+    bool is_word_valid(string word) {
+        return target_map.find(word) != target_map.end();
+    }
+
+    bool is_count_valid(string word) {
+        return window_map[word] < target_map[word];
+    }
+
+    void push(string word) {
+        window_map[word] += 1;
+        window_count += 1;
+    }
+
+    void pop(string word) {
+        window_map[word] -= 1;
+        window_count -= 1;
+    }
+
+    bool is_complete() {
+        return target_count == window_count;
+    }
+
+    void reset() {
+        window_map.clear();
+        window_count = 0;
+    }
+};
+
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        unordered_map<char, int> map;
-        for (char c : s) {
-            map[c] += 1;
-        }
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> ans;
+        if (s.empty() || words.empty()) return ans;
 
-        for (char c : t) {
-            map[c] -= 1;
-            if (map[c] == 0) {
-                map.erase(c);
+        int word_len = words[0].length();
+        unordered_map<string, int> target_map;
+        for (const string& word : words) {
+            target_map[word] += 1;
+        }
+        
+        for (int offset = 0; offset < words[0].length(); offset += 1) {
+            Window window = Window(target_map, words.size());
+
+            int left = offset;
+            for (int right = left; right + word_len <= s.length(); right += word_len) {
+                string word = s.substr(right, word_len);
+
+                if (!window.is_word_valid(word)) {
+                    // reset window
+                    window.reset();
+                    left = right + word_len;
+                    continue;
+                }
+
+                if (!window.is_count_valid(word)) {
+                    while (!window.is_count_valid(word)) {
+                        window.pop(s.substr(left, word_len));
+                        left += word_len;
+                    }
+                }
+
+                window.push(word);
+
+                if (window.is_complete()) {
+                    ans.push_back(left);
+                    window.pop(s.substr(left, word_len));
+                    left += word_len;
+                }
             }
         }
-
-        return map.size() == 0;
+        return ans;
     }
 };
 
