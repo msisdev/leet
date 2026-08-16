0ee04791a9aa4acd54134c7fcb27401004053ed8
msisdev <33077839+msisdev@users.noreply.github.com>
2026-08-16 12:51:19 +0900

    0104 maximum depth of binary tree

---
+++ leet-cpp/problems/p0104_maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,42 @@
+#include "problems/p0104_maximum_depth_of_binary_tree.hpp"
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+
+void run_p0104_demo() {
+    // TODO: Add local demo cases if needed.
+}
